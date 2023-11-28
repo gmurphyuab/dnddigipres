67e452097ec00d3a9ccd6ea570d59065581d5e74
gmurphyuab <132713602+gmurphyuab@users.noreply.github.com>
2023-11-28 11:54:03 -0600

    Update 1_FolderInventoryLoader.py
    De-clunkified code

--- DroboProject/1_FolderInventoryLoader.py
+++ DroboProject/1_FolderInventoryLoader.py
@@ -1,38 +1,30 @@
 import os
 import csv
 from datetime import datetime
-
-# Specify the directory to start walking from
-starting_directory = input('Enter the path to the directory: ')
-
-# Get the current date and year
-current_datetime = datetime.now().strftime('%Y%m%d_%H%M%S')
-
-# Create a CSV file name with the current day and year
-csv_filename = f'Folder_inventory_{current_datetime}.csv'
-
-# Function to walk through the directory and write to CSV
-def walk_directory_and_write_to_csv(directory, csv_writer):
-    for root, dirs, _ in os.walk(directory):
-        for folder_name in dirs:
-            # Construct the full path of the folder
-            folder_path = os.path.join(root, folder_name)
-            
-            # Write the directory path and folder name to the CSV
-            csv_writer.writerow([root, folder_name])
-
-# Open the CSV file for writing
-with open(csv_filename, mode='w', newline='') as csv_file:
-    csv_writer = csv.writer(csv_file)
-    
-    # Write headers to the CSV file
-    csv_writer.writerow(['Directory Path', 'Folder Name', 'UUID'])
-    
-    # Walk through the directory and write to CSV
-    walk_directory_and_write_to_csv(starting_directory, csv_writer)
-
-print(f"CSV file '{csv_filename}' has been created with directory paths and folder names.")
-
-# Opening the CSV file with the default system application
 import subprocess
-subprocess.Popen(['start', 'excel', csv_filename], shell=True)
+
+def create_inventory_csv(directory, csv_writer):
+    items = os.listdir(directory)
+    sorted_data = sorted(items, key=lambda x: int(''.join(filter(str.isdigit, x))))
+    for item in sorted_data:
+        item_path = os.path.join(directory, item)
+        if os.path.isdir(item_path):
+            csv_writer.writerow([directory, item])
+
+def main():
+
+    starting_directory = input('Enter the path to the directory: ')
+    current_datetime = datetime.now().strftime('%Y%m%d_%H%M%S')
+    csv_filename = f'Folder_inventory_{current_datetime}.csv'
+
+    with open(csv_filename, mode='w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(['Directory Path', 'Folder Name', 'UUID'])
+        create_inventory_csv(starting_directory, csv_writer)
+
+    print(f"CSV file '{csv_filename}' has been created with directory paths and folder names.")
+    subprocess.Popen(['start', 'excel', csv_filename], shell=True)
+
+if __name__ == "__main__":
+    main()
+
