d134154c4f1f78cdcd04a9fb91fa97ddf67622ea
gmurphyuab <132713602+gmurphyuab@users.noreply.github.com>
2023-11-29 08:52:24 -0600

    Update 2_RenameFolder.py

--- DroboProject/2_RenameFolder.py
+++ DroboProject/2_RenameFolder.py
@@ -3,7 +3,6 @@ import csv
 import tkinter as tk
 from tkinter import filedialog
 
-# Function to rename folders based on CSV data
 def rename_folders_from_csv(csv_file_path):
     with open(csv_file_path, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -12,11 +11,8 @@ def rename_folders_from_csv(csv_file_path):
         for row in csv_reader:
             if len(row) == 3:
                 folder_path, existing_name, new_name = row
-
-                # Construct the full path to the folder
                 full_path = os.path.join(folder_path, existing_name)
 
-                # Check if the folder exists before renaming
                 if os.path.exists(full_path):
                     new_full_path = os.path.join(folder_path, new_name)
                     try:
@@ -27,36 +23,27 @@ def rename_folders_from_csv(csv_file_path):
                 else:
                     result_label.config(text=f"Folder '{existing_name}' in '{folder_path}' does not exist.")
 
-# Function to browse for a CSV file
 def browse_for_csv():
     file_path = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
     if file_path:
         csv_entry.delete(0, tk.END)
         csv_entry.insert(0, file_path)
-
-# Create the main window
+#GUI
 root = tk.Tk()
 root.title("Folder Renamer")
 
-# Create and configure the label for CSV file selection
 csv_label = tk.Label(root, text="Select CSV File:")
 csv_label.pack()
-
-# Create and configure the entry field for CSV file path
 csv_entry = tk.Entry(root, width=50)
 csv_entry.pack()
 
-# Create and configure the Browse button
 browse_button = tk.Button(root, text="Browse", command=browse_for_csv)
 browse_button.pack()
 
-# Create and configure the Rename button
 rename_button = tk.Button(root, text="Rename Folders", command=lambda: rename_folders_from_csv(csv_entry.get()))
 rename_button.pack()
 
-# Create and configure the result label
 result_label = tk.Label(root, text="")
 result_label.pack()
 
-# Start the Tkinter main loop
 root.mainloop()
