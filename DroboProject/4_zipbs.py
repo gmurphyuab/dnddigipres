a36466a33535f0ce8a46116a467fd0ebfe04f8a1
gmurphyuab <132713602+gmurphyuab@users.noreply.github.com>
2023-11-29 08:55:29 -0600

    Update 4_zipbs.py

--- DroboProject/4_zipbs.py
+++ DroboProject/4_zipbs.py
@@ -1,7 +1,6 @@
 import os
 import zipfile
 
-# Function to create a zip file for a folder excluding .txt files
 def create_zip_for_folder(folder_path, output_dir):
     folder_name = os.path.basename(folder_path)
     zip_filename = os.path.join(output_dir, f"{folder_name}.zip")
@@ -14,13 +13,10 @@ def create_zip_for_folder(folder_path, output_dir):
                     arcname = os.path.relpath(file_path, folder_path)
                     zipf.write(file_path, arcname)
 
-# Specify the input directory where the folders are located
-input_directory = input('Enter the input directory to zip:')
+input_directory = input('Enter the input directory of folders to zip:')
 
-# Specify the output directory where the zip files will be saved
 output_directory = input('Enter the output directory: ')
 
-# Iterate over folders in the input directory
 for folder in os.listdir(input_directory):
     folder_path = os.path.join(input_directory, folder)
     if os.path.isdir(folder_path):
